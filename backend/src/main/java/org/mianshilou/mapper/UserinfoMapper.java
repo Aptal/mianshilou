ea21e20c0717cf719790a5fb1cc60fcb3d42d07f
Aptal <53370626+Aptal@users.noreply.github.com>
2022-06-22 20:39:19 +0800

    Add files via upload

---
+++ backend/src/main/java/org/mianshilou/mapper/UserinfoMapper.java
@@ -0,0 +1,9 @@
+package org.mianshilou.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.mianshilou.pojo.Userinfo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserinfoMapper extends BaseMapper<Userinfo> {
+}
